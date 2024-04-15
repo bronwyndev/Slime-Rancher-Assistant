f9846bafbfa6f5613939e0fa0a5e8d18189ac585
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-04-15 10:49:18 +1000

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,22 @@
+import {defineConfig, isDev} from 'sanity'
+import {visionTool} from '@sanity/vision'
+import {structureTool} from 'sanity/structure'
+import {schemaTypes} from './schemaTypes'
+import {getStartedPlugin} from './plugins/sanity-plugin-tutorial'
+
+const devOnlyPlugins = [getStartedPlugin()]
+
+export default defineConfig({
+  name: 'default',
+  title: 'Slime Rancher Assistant',
+
+  projectId: '9yc4oudn',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool(), ...(isDev ? devOnlyPlugins : [])],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
+
