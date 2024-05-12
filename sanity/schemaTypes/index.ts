001ff9ee97b418d05b73ec01fdd6eb2eeb49019e
Bronwyn Waterhouse <bronwyn.waterhouse@outlook.com>
2024-05-12 23:48:59 +1000

    Add treasure pod type to index

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -4,5 +4,6 @@ import {buildingType} from './buildingType'
 import {gadgetType} from './gadgetType'
 import {gordoType} from './gordoType'
 import {resourceType} from './resourceType'
+import {treasurePodType} from './treasurePodType'
 
-export const schemaTypes = [slimeType, foodType, buildingType, gadgetType, gordoType, resourceType]
+export const schemaTypes = [slimeType, foodType, buildingType, gadgetType, gordoType, resourceType, treasurePodType]
